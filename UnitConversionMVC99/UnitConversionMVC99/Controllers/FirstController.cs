4f14c33d7ee66c332cfed0914ede807aac7fde73
Unknown <19brunel@elmbrookstudents.org>
2018-06-21 14:43:11 -0500

    All of work

--- UnitConversionMVC99/UnitConversionMVC99/Controllers/FirstController.cs
+++ UnitConversionMVC99/UnitConversionMVC99/Controllers/FirstController.cs
@@ -12,5 +12,30 @@ namespace UnitConversionMVC99.Controllers
         {
             return View();
         }
+        public IActionResult MilesKm()
+        {
+            List<double> inputsF = new List<double>(2) {0d,0d };
+            return View(viewName: "MilesKm", model: inputsF);
+        }
+
+        [HttpPost]
+        public ActionResult MilesKm(double input1, double input2)
+        {
+            if (input1 == 0 || input2 == 0)
+            {
+                if (input1 == 0)
+                {
+                    input1 = input2 / 1.609;
+                }
+                if (input2 == 0)
+                {
+                    input2 = input1 * 1.609;
+                }
+            }
+            List<double> inputs = new List<double>();
+            inputs.Add(input1);
+            inputs.Add(input2);
+            return View(viewName: "MilesKm", model: inputs);
+        }
     }
 }
