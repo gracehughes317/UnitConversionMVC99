bae1d2710d12dc26a355da5a14c34686cb819c9b
Pranay Reddy <19reddyp@elmbrookstudents.org>
2018-06-22 09:27:58 -0500

    redid my code

--- UnitConversionMVC99/UnitConversionMVC99/Controllers/FirstController.cs
+++ UnitConversionMVC99/UnitConversionMVC99/Controllers/FirstController.cs
@@ -8,9 +8,32 @@ namespace UnitConversionMVC99.Controllers
 {
     public class FirstController : Controller
     {
-        public IActionResult Index()
+        public ActionResult CmtoIn()
         {
-            return View();
+            List<double> bob = new List<double>(2) { 0d, 0d };
+            return View(viewName: "CmtoIn", model: bob);
         }
-    }
+      
+        [HttpPost]
+        public ActionResult cmtoinches(double inches, double cm)
+        {
+            List<double> bob = new List<double>();
+            if (inches == 0 || cm == 0)
+            {
+                if (inches == 0)
+                {
+                    inches = cm / 2.54;
+                }
+
+                if (cm == 0)
+                {
+                    cm = inches * 2.54;
+                }
+                bob.Add(inches);
+                bob.Add(cm);
+            }
+            return View(viewName: "CmtoIn", model: bob);
+        }
+
+   }
 }
