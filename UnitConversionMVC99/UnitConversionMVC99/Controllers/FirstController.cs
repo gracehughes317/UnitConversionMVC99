128b540f99d2d4b710ab1f82abcd5c91fc0f2073
Unknown <19brunel@elmbrookstudents.org>
2018-06-26 09:35:52 -0500

    Fixing controller

--- UnitConversionMVC99/UnitConversionMVC99/Controllers/FirstController.cs
+++ UnitConversionMVC99/UnitConversionMVC99/Controllers/FirstController.cs
@@ -21,61 +21,11 @@ namespace UnitConversionMVC99.Controllers
             list.Add(0d);
             return View(viewName: "GallonsLiters", model: list);
         }
-
-        [HttpPost]
-        public IActionResult ConvertGL(double gallons, double liters)
-        {
-            List<double> units = new List<double>();
-            if (gallons != 0)
-            {
-                units.Add(gallons);
-                units.Add(gallons * 3.78541);
-            }
-            else if (liters != 0)
-            {
-                units.Add(liters / 3.78541);
-                units.Add(liters);
-            }
-            return View(viewName: "GallonsLiters", model: units);
-        }
-    }
-}
-      
-        [HttpPost]
-        public ActionResult cmtoinches(double inches, double cm)
-        {
-            List<double> bob = new List<double>();
-            if (inches == 0 || cm == 0)
-            {
-                if (inches == 0)
-                {
-                    
-                   double val1 = cm / 2.54;
-                   inches= Math.Round(1000 * val1) / 1000;
-                }
-
-                if (cm == 0)
-                {
-                    double val1 = inches * 2.54;
-                    cm = Math.Round(1000 * val1) / 1000;
-                }
-                bob.Add(inches);
-                bob.Add(cm);
-            }
-            return View(viewName: "CmtoIn", model: bob);
         public IActionResult MilesKm()
         {
-            List<double> inputsF = new List<double>(2) {0d,0d };
+            List<double> inputsF = new List<double>(2) { 0d, 0d };
             return View(viewName: "MilesKm", model: inputsF);
         }
-        public IActionResult GallonsLiters()
-        {
-            List<double> list = new List<double>();
-            list.Add(0d);
-            list.Add(0d);
-            return View(viewName: "GallonsLiters", model: list);
-
-   }
         [HttpPost]
         public ActionResult MilesKm(double input1, double input2)
         {
@@ -110,4 +60,31 @@ namespace UnitConversionMVC99.Controllers
             }
             return View(viewName: "GallonsLiters", model: units);
         }
+        public ActionResult cmtoinches(double inches, double cm)
+        {
+            List<double> bob = new List<double>();
+            if (inches == 0 || cm == 0)
+            {
+                if (inches == 0)
+                {
+
+                    double val1 = cm / 2.54;
+                    inches = Math.Round(1000 * val1) / 1000;
+                }
+
+                if (cm == 0)
+                {
+                    double val1 = inches * 2.54;
+                    cm = Math.Round(1000 * val1) / 1000;
+                }
+                bob.Add(inches);
+                bob.Add(cm);
+            }
+            return View(viewName: "CmtoIn", model: bob);
+        }
+
+
+
     }
+}
+
